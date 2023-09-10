000f87aff5183a37b29dc1e3c474e0de2d9080cc
ali_bakhsha <ali.bakhshayesh@gmail.com>
2023-09-10 19:05:38 +0330

    added

--- Algorithms/STL_SORT/sampleCode.cpp
+++ Algorithms/STL_SORT/sampleCode.cpp
@@ -18,8 +18,24 @@
 #include <algorithm>
 #include <execution>
 using namespace std;
-int main(){
+
+class Point{
+	public:
+	int x, y;
 	
+	Point(int x , int y):x(x) , y(y){}
+	bool operator <(const Point& p1){ return (x+y) < (p1.x+p1.y);}
+	bool operator >(const Point& p1){ return (x+y) > (p1.x+p1.y);}
+};
+
+struct{
+	bool operator()(int a , int b) const
+	{
+		return a < b;
+	}
+}customLess;
+int main(){
+	{
 	std::vector<int> vec{4,3,5,2,6,1};
 	
 	std::sort(vec.begin() , vec.end());
@@ -28,7 +44,35 @@ int main(){
 	std::sort(std::execution::par , vec.begin() , vec.end() , std::greater<int>());
 	for(auto& elm : vec)
 		cout << elm << " ";
+	}
+	cout << endl<< endl;
+	// 2. Sortng user defined data types
+
+	{
+	std::vector<Point> vec{{1,2},{3,1},{0,1}};
+	
+	std::sort(vec.begin() , vec.end());
+	//std::sort(vec.begin() , vec.end() , std::greater<Point>());
+	//std::sort(std::execution::par , vec.begin() , vec.end());
+	//std::sort(std::execution::par , vec.begin() , vec.end() , std::greater<Point>());
+	for(auto& elm : vec)
+		cout << elm.x << " " << elm.y << endl;
+	}
+		cout << endl<< endl;
+		
+	// 3. Sort using a function object
+	{
+	std::vector<int> vec{4,3,5,2,6,1};
+	
+	std::sort(vec.begin() , vec.end());
+	std::sort(vec.begin() , vec.end() , customLess);
+	std::sort(vec.begin() , vec.end() , [](int a, int b){return a<b;});
+	for(auto& elm : vec)
+		cout << elm << " ";
+	}
 	
 	
+	// 4. Sort using lambda expression
+
 	return 0;
 }
